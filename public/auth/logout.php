1ef95e666676f2b2acbb9dd0ddb0df33dadfdfea
GiriPurushotam <giripurushotam39@gmail.com>
2026-01-26 22:31:21 +0545

    Redirect user's to index page instead of login after logout

--- public/auth/logout.php
+++ public/auth/logout.php
@@ -1,4 +1,4 @@
 <?php
 require_once __DIR__ . '/../../includes/functions.php';
 logoutUser();
-header('Location: login.php?success_auth=Logged Out');
+header('Location: /movie_project/public/index.php');
